37eb9f940f60d0048b5f12734bdf871a81651c74
cpcoats <cpcoats@gmail.com>
2014-01-26 15:54:17 -0500

    when event is put() event list is create/updated using boardgamegeek api and save a list of all games owned by host

--- models/events.py
+++ models/events.py
@@ -1,5 +1,11 @@
 from google.appengine.ext import ndb
 
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
+import urllib2
+import xml.dom.minidom as mdom
+
+
 class Events(ndb.Model):
     title = ndb.StringProperty()
     date = ndb.DateProperty()
@@ -8,11 +14,28 @@ class Events(ndb.Model):
     games = ndb.StringProperty(repeated=True)
 
     def _pre_put_hook(self):
-        print("pre put hook")
-        print("self is...")
-        print(self)
         #update game list
         self.update_gamelist()
 
     def update_gamelist(self):
-        print(self)
+
+        host_bggUsername = "abishek_sm"
+
+        requestURL = "http://www.boardgamegeek.com/xmlapi/collection/" \
+                             + host_bggUsername + "?own=1"
+
+        data = urlfetch.fetch(requestURL).content
+        dom = mdom.parseString(data)
+
+        #loop through dom and get all games
+
+        items = dom.getElementsByTagName("item")
+
+        game_titles = []
+        for item in items:
+            item_name = item.getElementsByTagName('name')
+            for i in item_name:
+                game_titles.append(i.firstChild.data)
+
+        self.games = game_titles
+
