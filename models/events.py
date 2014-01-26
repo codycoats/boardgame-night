45ce0a86aa68e3b1230da5937d7055f1146c2633
cpcoats <cpcoats@gmail.com>
2014-01-26 13:21:30 -0500

    require login to create/edit/delete events

--- models/events.py
+++ models/events.py
@@ -3,3 +3,5 @@ from google.appengine.ext import ndb
 class Events(ndb.Model):
     title = ndb.StringProperty()
     date = ndb.DateProperty()
+    host = ndb.UserProperty(required=True)
+    attendees = ndb.UserProperty(repeated=True)
