c78254c7193af0688cc35c3a82a8c34a51670d7a
cpcoats <cpcoats@gmail.com>
2014-02-05 15:05:50 -0500

    game list on event is now separated by playable and unplayabel games

--- static/js/events.js
+++ static/js/events.js
@@ -2,8 +2,34 @@ $( function() {
 
     $('#event-date').datepicker();
 
-    //check if event is playable
-    $.each
+    //mark unplayable games
+    games = $('.game').detach();
+    var playable =[];
+    var unplayable = [];
+    numAttendees = $("#attendeeCount_badge").data('count')
 
+    $.each(games, function(item, game){
+        if (numAttendees > $(game).data('maxplayers')) {
+            $(game).attr('data-playable', false);
+            $(this).addClass('list-group-item-danger');
+            unplayable.push(game);
+        }
+        else if(numAttendees < $(game).data('minplayers')){
+            $(game).attr('data-playable', false);
+            $(this).addClass('list-group-item-danger');
+            unplayable.push(game);
+        }
+        else{
+            playable.push(game);
+        }
+    });
+
+
+    //separate lists
+    console.log(playable);
+    console.log(unplayable);
+
+    $('#playable-list').append(playable);
+    $('#unplayable-list').append(unplayable);
 });
 
